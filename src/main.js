5e84ec8d7c299afa7037981911cee81c33d5f4a8
qing-2016 <73535346+qing-2016@users.noreply.github.com>
2020-10-28 18:32:17 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false //123
-
+Vue.config.productionTip = false //22
+//asdsda
 new Vue({
   render: h => h(App),
 }).$mount('#app')
